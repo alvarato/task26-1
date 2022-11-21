1aba016543871375ec39f67814102c52386d9c8d
alvaro <bernabeyalvaro@gmail.com>
2022-11-21 13:30:47 +0100

    security add

--- UD26-1/src/main/java/com/pacoteck/springboot/app/controller/ProveedorController.java
+++ UD26-1/src/main/java/com/pacoteck/springboot/app/controller/ProveedorController.java
@@ -3,6 +3,7 @@ package com.pacoteck.springboot.app.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -24,7 +25,7 @@ public class ProveedorController {
 		return imp.findAll();
 	}
 	
-	@PutMapping("/delete{id}")
+	@DeleteMapping("/delete{id}")
 	public void deleteById(@PathVariable("id") int id) {
 		imp.deleteById(id);
 	}
